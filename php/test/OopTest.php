12fb34bdaac1028751ff9092725592320af831fa
Jim Neuendorf <jim.neuendorf@gmx.de>
2017-05-13 23:52:20 +0200

    code/comment cleaning, create __BaseClass to hold common functionality for all (otherwise) superclasses

--- php/test/OopTest.php
+++ php/test/OopTest.php
@@ -35,6 +35,7 @@ class OopTest extends TestCase {
         assertEquals($b1->b(), 2);
         assertEquals($b2->b(), 2);
 
+        // in order to have `$self` the according parameter must be passed
         $b1->b = function($self) {
             return super(get_class($self), $self)->b() + 3;
         };
@@ -44,31 +45,11 @@ class OopTest extends TestCase {
         assertEquals($b2->b(), 2);
     }
 
-    // public function testChangingInstanceMethodsOn2Class() {
-    //     $a1 = new A();
-    //     $a2 = new A();
-    //     $b1 = new B();
-    //     $b2 = new B();
-    //
-    //     // echo 'initial call', $a1->b(), "\n";
-    //     assertEquals($a1->b(), 1);
-    //     assertEquals($a2->b(), 1);
-    //     assertEquals($b2->b(), 2);
-    //     assertEquals($b2->b(), 2);
-    //     // set function property (it is no method anymore)
-    //     $a1->b = function() {
-    //         return 3;
-    //     };
-    //     assertEquals($a1->b(), 3);
-    //     assertEquals($a2->b(), 1);
-    //     assertEquals($b2->b(), 2);
-    //     assertEquals($b2->b(), 2);
-    //
-    //     $b1->b = function($self) {
-    //         return super('B', $self)->b() + 3;
-    //     };
-    //     assertEquals($b1->b($b1), 4);
-    // }
+    public function testClassMethods() {
+        assertEquals(A::classmethod(), 'A');
+        assertEquals(B::classmethod(), 'B');
+    }
+
 }
 
 
--- php/test/oop.php
+++ php/test/oop.php
@@ -1,18 +1,79 @@
 <?php
 
-// function exception_error_handler($errno, $errstr, $errfile, $errline) {
-//     throw new \ErrorException($errstr, $errno, 0, $errfile, $errline);
-// }
-// set_error_handler("exception_error_handler");
-
+if (!function_exists('exception_error_handler')) {
+    function exception_error_handler($errno, $errstr, $errfile, $errline) {
+        throw new \ErrorException($errstr, $errno, 0, $errfile, $errline);
+    }
+    set_error_handler("exception_error_handler");
+}
 
 
 class AttributeError extends \Exception {}
 
-class __Class {
+class __Class {}
 
+// All classes written in python inherit from this class
+// It is not part of the static `__mro__` attribute.
+class __BaseClass {
+    public function __call($name, $arguments) {
+        echo '__call(): ', $name, ' on instance of ', get_class($this),  "\n";
+        if (array_key_exists($name, $this->__properties)) {
+            echo 'using $this->__properties', "\n";
+            // don't pass implicit `self` because this property is a function and not a method
+            return $this->__properties[$name](...$arguments);
+            // return $this->__properties[$name]($this, ...$arguments);
+        }
+        // var_dump($this);
+        // var_dump(get_called_class());
+        // var_dump(static::$__mro__);
+        foreach (static::$__mro__ as $cls) {
+            // dynamically set (on this instance only)
+            // echo '>>> ', $name, ' ', $cls, "\n";
+            // var_dump($cls);
+            // echo "\n";
+            // not dynamically set for this instance => defined on the class => use global
+            if (array_key_exists($cls."_".$name, $GLOBALS)) {
+                echo "> global ".$cls."_".$name." exists\n";
+                return $GLOBALS[$cls."_".$name]($this, ...$arguments);
+            }
+        }
+        throw new \AttributeError('\''.get_class($this).'\' has no attribute \''.$name.'\'', 1);
+    }
+
+    public static function __callStatic($name, $arguments) {
+        // $func = static::$__class_properties[$name];
+        // return $func(get_class(), ...$arguments);
+        echo '__callStatic(): ', $name, ' on ', get_called_class(),  "\n";
+        if (array_key_exists($name, static::$__class_properties)) {
+            echo 'using static::$__class_properties', "\n";
+            $func = static::$__class_properties[$name];
+            // don't pass implicit `self` because this property is a function and not a method
+            return $func(...$arguments);
+        }
+        foreach (static::$__mro__ as $cls) {
+            // dynamically set (on this instance only)
+            // echo '>>> ', $name, ' ', $cls, "\n";
+            // var_dump($cls);
+            // echo "\n";
+            // not dynamically set => statically defined => use global
+            if (array_key_exists($cls."_class_".$name, $GLOBALS)) {
+                echo "> global ".$cls."_class_".$name." exists\n";
+                return $GLOBALS[$cls."_class_".$name](get_called_class(), ...$arguments);
+            }
+        }
+        throw new \AttributeError('\''.get_called_class().'\' has no attribute \''.$name.'\'', 1);
+    }
+
+    public function __set($name, $value) {
+        $this->__properties[$name] = $value;
+    }
+
+    public function __setStatic($name, $value) {
+        $this->__class_properties[$name] = $value;
+    }
 }
 
+// This one's handling `super()` calls.
 class Super {
     // the instance invoking super(); may be None
     public $__self__;
@@ -33,7 +94,6 @@ class Super {
             $__self_class__::$__mro__,
             array_search($__self_class__, $__self_class__::$__mro__) + 1
         );
-
         // echo "constructing new Super():\n";
         // var_dump($this);
     }
@@ -42,27 +102,13 @@ class Super {
         // echo 'super() >>> ', $name, '  ';
         // var_dump($this->__self__);
         foreach ($this->_mro as $cls) {
-            // dynamically set (on this instance only)
             // var_dump($cls);
             // echo "\n";
-            // if `$this->__self__->__properties` doesn't exist it means we're in the `__init` method
-            // if (array_key_exists($name, $this->__self__->__properties)) {
-            //     // // don't pass implicit `self` because this property is a function and not a method
-            //     // echo 'args = ';
-            //     // var_dump($arguments);
-            //     // return $this->__self__->__properties[$name](...$arguments);
-            //     return $this->__self__->__properties[$name]($this->__self__, ...$arguments);
-            // }
-            // else
-            // not dynamically set => defined as method => use global
+            // `isset($GLOBALS[$cls."_".$name])` is `True` => use instance method of `$cls`
             if (array_key_exists($cls."_".$name, $GLOBALS)) {
                 // echo "> global ".$cls."_".$name." exists\n";
                 return $GLOBALS[$cls."_".$name]($this->__self__, ...$arguments);
             }
-            // else if (method_exists($this->__self__, $name)) {
-            //     echo "> method exists\n";
-            //     return $this->__self__->$name($this->__self__, ...$arguments);
-            // }
             echo "using next class in mro...\n";
         }
         throw new \AttributeError('\''.$this->__self_class__.'\' has no attribute \''.$name.'\'', 1);
@@ -96,17 +142,12 @@ function set_attr($object, $name, $value) {
 }
 
 
-
 // instance method compiled from `class A: def b(): pass`
 // Changing this variable means changing the method on the class:
 // class A:
 //     def b():
 //         return 1
 // A.b = lambda: 2
-// $GLOBALS['A_b'] = function($self) {
-//     return 1;
-// };
-global $A_b;
 $A_b = function($self) {
     return 1;
 };
@@ -115,8 +156,7 @@ $A_class_classmethod = function($self) {
     return $self;
 };
 
-class A {
-    // // array keys of these 2 arrays are disjoint
+class A extends __BaseClass {
     // holds class properties that were set dynamically
     public static $__class_properties;
     public static $__mro__;
@@ -125,13 +165,10 @@ class A {
 
     public static function __init_static() {
         self::$__mro__ = ['A'];
-        // static::$__class_properties_name = '__'.static::$__prefix.'_class_properties';
-        // $__class_properties_name = static::$__class_properties_name;
-        // TODO: Use this method to init classmethods etc.
         self::$__class_properties = [
-            'classmethod' => function($self) {
-                return $self;
-            },
+            // 'classmethod' => function($self) {
+            //     return $self;
+            // },
         ];
     }
 
@@ -149,44 +186,6 @@ class A {
         // 2.: normal body (compiled from python)
         // ...
     }
-
-    public function __call($name, $arguments) {
-        echo '__call()', $name, ' on instance of ', get_class($this),  "\n";
-        // var_dump($this);
-        // var_dump(get_called_class());
-        // var_dump(static::$__mro__);
-        foreach (static::$__mro__ as $cls) {
-            // dynamically set (on this instance only)
-            // echo '>>> ', $name, ' ', $cls, "\n";
-            // var_dump($cls);
-            // echo "\n";
-            if (array_key_exists($name, $this->__properties)) {
-                echo 'using $this->__properties', "\n";
-                // don't pass implicit `self` because this property is a function and not a method
-                return $this->__properties[$name](...$arguments);
-                // return $this->__properties[$name]($this, ...$arguments);
-            }
-            // not dynamically set => statically defined => use global
-            else if (array_key_exists($cls."_".$name, $GLOBALS)) {
-                echo "> global ".$cls."_".$name." exists\n";
-                return $GLOBALS[$cls."_".$name]($this, ...$arguments);
-            }
-        }
-        throw new \AttributeError('\''.get_class($this).'\' has no attribute \''.$name.'\'', 1);
-    }
-
-    public static function __callStatic($name, $arguments) {
-        $func = static::$__class_properties[$name];
-        return $func(get_class(), ...$arguments);
-    }
-
-    public function __set($name, $value) {
-        $this->__properties[$name] = $value;
-    }
-
-    public function __setStatic($name, $value) {
-        $this->__class_properties[$name] = $value;
-    }
 }
 A::__init_static();
 
@@ -202,52 +201,20 @@ class B extends A {
     public $__properties;
 
 
-    // public function __construct() {
-    //     $this->__init();
-    // }
-
-    public static function __init_static() {
-        // self::$__B_mro = ['B', 'A'];
-        self::$__mro__ = ['B', 'A'];
+    public function __construct() {
+        $this->__init();
     }
 
-    // public function __init() {
-    //     // super(__CLASS__, $this)->__init();
-    //     $this->__properties = [];
-    //     // $this->__properties = array_merge(
-    //     //     $this->__properties,
-    //     //     [
-    //     //         'b' => function($self) {
-    //     //             var_dump(get_parent_class());
-    //     //             // return $this->__call_super('b') + 1;
-    //     //             return super(__CLASS__, $this)->b() + 1;
-    //     //             // return parent::b() + 1;
-    //     //         },
-    //     //     ]
-    //     // );
-    // }
+    public static function __init_static() {
+        self::$__mro__ = ['B', 'A'];
+        self::$__class_properties = [];
+    }
+
+    public function __init() {
+        $this->__properties = [];
+    }
 }
-
 B::__init_static();
-
-
-// echo "===> A\n";
-//
-// $a1 = new A();
-// $a2 = new A();
-// echo 'initial call', $a1->b(), "\n";
-// // set function property (it is no method anymore)
-// $a1->b = function() {
-//     return 3;
-// };
-// echo 'after function property was set ', $a1->b(), "\n\n";
-// echo 'a2.b() ', $a2->b(), "\n\n";
-//
-// echo "===> B\n";
-//
-// $b = new B();
-// $result = $b->b();
-// echo 'result = ', $result, "\n\n";
 
 
 // echo A::classmethod(), "\n";
